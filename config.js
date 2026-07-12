0101d8ac29c38f44823fe22369b5eb4fe809c24d
motiarrahman9483-sys <motiarrahman9483@gmail.com>
2026-07-13 00:19:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ const PRODUCT = {
         "assets/images/product2.jpg",
         "assets/images/product3.jpg",
        "assets/images/product4.jpg"
-        "assets/images/product4.jpg"
+        "assets/images/product5.jpg"
     ],
 
     // =========================
